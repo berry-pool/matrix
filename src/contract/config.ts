2a830b0189ca9b0327d2e0b651d5f86ac33db4fb
alessandrokonrad <akonrad6@gmail.com>
2022-10-27 18:08:08 +0200

    set contract params

--- src/contract/config.ts
+++ src/contract/config.ts
@@ -1,18 +1,18 @@
 export const contractDetails = {
   controlOref: {
-    txHash: "733b5b0a83e84013ce3064d003a39bda119ed3400e98bd5057baf83bac672253",
-    outputIndex: 4,
+    txHash: "70166bfe55c1c5001982f45794abb44254bb81cfc71eba7e3636b6fd05cecfa9",
+    outputIndex: 1,
   },
-  berryPolicyId: "a65e6e94d1a260dbc6c4d9319b45585fa54b83742a33a2c599df56b9",
+  berryPolicyId: "b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f",
   payeeAddress:
-    "addr_test1qqdr6txha8u2q4c5h2xy5rvk7lslvr252k2khln5mea32lmnha7lkynauew8mp5v945533qsmaj9l9jgqjz3d9fkjmtq9c7rvm",
+    "addr1q8c0de89nt8sukm52avpzem04ua0ucf0fn4m7jylelvazh6hcq9dmd8wqc9wlfyune7wxd5w65hfftxhnl704fsnwsxqsfmlmg",
   paymentAmount: 1300000000n,
   mintStart: 1667488718827,
   royaltyOref: {
-    txHash: "4d6440538af7e9f03dba4723b03bae9c4fa23bd93027cf57498564426d78cd39",
-    outputIndex: 0,
+    txHash: "dc2a273465fc362edb073efb0d079fc0b7b513597fccc3c49c3522b0d6505c97",
+    outputIndex: 2,
   },
-  controlOwner: "0af00cc47500bb64cfffb783e8c42f746b4e8b8a70ede9c08c7113ac",
+  controlOwner: "d52e11f3e48436dd42dbec6d88c239732e503b8b7a32af58e5f87625",
   txValidFromThreshold: 100000,
   txValidToThreshold: 2000000,
   referenceScriptsTxHash:
