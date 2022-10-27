d798f128333a868470c7124f7059d77e0db53ace
alessandrokonrad <akonrad6@gmail.com>
2022-10-27 23:02:07 +0200

    fixed minted

--- src/pages/alreadyMinted.tsx
+++ src/pages/alreadyMinted.tsx
@@ -1,6 +1,21 @@
 import * as React from "react";
-import { getAllMintedIds } from "../contract/offchain";
 import metadata from "../data/berryAssigned.json";
+import { projectId } from "../../secrets";
+import { fromUnit, hexToUtf8 } from "lucid-cardano";
+
+export const getAllMintedIds = async (): Promise<number[]> => {
+  const matrixAssets = await fetch(
+    `https://cardano-mainnet.blockfrost.io/api/v0/assets/policy/${"01cecfaeda9d846c08675902b55a6371f593d9239744867462c5382e"}`,
+    { headers: { project_id: projectId } }
+  ).then((r) => r.json());
+  if (!matrixAssets || matrixAssets.error) return [];
+
+  return matrixAssets
+    .filter((asset: any) => fromUnit(asset.asset).label === 100)
+    .map((asset: any) =>
+      parseInt(hexToUtf8(fromUnit(asset.asset).name!).slice(6))
+    );
+};
 
 const AlreadyMinted = () => {
   const [minted, setMinted] = React.useState<string[]>([]);
